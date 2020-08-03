89ea611b12a5474e3e7e41291396fd2395409bb1
donutrollshole <65697836+donutrollshole@users.noreply.github.com>
2020-08-03 18:47:04 -0400

    now works with one word searchterms

--- ebay-url.py
+++ ebay-url.py
@@ -1,4 +1,8 @@
 import requests
 
 def get_listing_urls(searchterm):
-    pass
+    s = requests.Session()
+    html_data = s.get(f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR12.TRC2.A0.H0.X{searchterm}.TRS0&_nkw={searchterm}&_sacat=0').text
+    return(html_data)
+
+print(get_listing_urls('i7'))
