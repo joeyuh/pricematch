3ffcf4b49291423b519b8a3eb69a40299823c911
donutrollshole <65697836+donutrollshole@users.noreply.github.com>
2020-08-07 00:08:14 -0400

    send-email can do image

--- send-email.py
+++ send-email.py
@@ -1,6 +1,7 @@
 def send_an_email(subject, receiver, message_content):
     import smtplib
     from email.message import EmailMessage
+    import imghdr
 
     email_address = 'gooddealscript@gmail.com'
     email_password = 'whataPassword'
@@ -11,8 +12,13 @@ def send_an_email(subject, receiver, message_content):
     msg['From'] = email_address
     msg['To'] = receiver
     msg.set_content(message_content)
-    
 
+    with open('test_image.jpg', 'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+    
+    msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(email_address, email_password)
