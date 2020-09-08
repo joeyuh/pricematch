57f30640948b7730723e47e4286466d6297330ea
donutrollshole <65697836+donutrollshole@users.noreply.github.com>
2020-09-07 22:55:55 -0400

    hws_scraper updated, works slightly better

--- hws_scraper.py
+++ hws_scraper.py
@@ -1,4 +1,8 @@
 import praw
+import re
+import requests
+#homemade modules below!
+import sendemail
 
 #I HAVE CREATED A NEW REDDIT ACCOUNT. USERNAME = 'pcbeest', PASSWORD = 'whataPassword'.
 
@@ -16,8 +20,23 @@ reddit = praw.Reddit(client_id = client_id,
 
 subreddit = reddit.subreddit('hardwareswap')
 
+s = requests.Session()
+useragent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0) Gecko/20190101 Firefox/77.0'}
 for submission in subreddit.new(limit=10):
-    print(submission.title)  # Output: the submission's title
-    print(submission.url)    # Output: the submission's URL
-    print(submission.link_flair_text)
-    print(submission.selftext)
+    listing_data = submission.selftext
+
+    if submission.link_flair_text == 'SELLING':
+        print(submission.url)
+        print(submission.link_flair_text)
+        print(submission.title)
+        print(submission.selftext)
+
+        #find imgur link url. People might timestamp in other sites, but ....
+        pattern = re.compile(r'https://imgur\.com/a/.......')
+        matches = pattern.findall(listing_data)
+        for match in matches: 
+            print(match)
+
+
+        print('\n\n\n')
+            
