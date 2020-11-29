5d878c156e56a85c6b286b1d686172ee5c3a5dc8
donutrollshole <65697836+donutrollshole@users.noreply.github.com>
2020-11-28 21:33:03 -0500

    fixed image search regex

--- hws_scraper1.py
+++ hws_scraper1.py
@@ -100,7 +100,7 @@ while True:
 
             #FIND TIMESTAMP LINKS IN THE POST'S SOURCE CODE AND ATTRIBUTE THEM TO THE CLASS INSTANCE
             source = webscraper.scrape_page(post_url)
-            search_string = r'href="(http(s)?://)?(i.)?(imgur\.com/(a/)?[\w\d]{5,7}|imgur.com/gallery/[\w\d]{5,7}|ibb.co/.{5,7})'
+            search_string = r'(?i)href="(http(s)?://)?(i.)?(imgur.com/gallery/[\w\d]{5,8}|imgur\.com/(a/)?[\w\d]{5,7}|ibb.co/.{5,7})'
             timestamp_urls = re.finditer(search_string, str(source))
             post_items.timestamp = []
             for match in timestamp_urls:
